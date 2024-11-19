ed951fd89a10e876e835b37dd88524fec21efe76
drooschuck <114895991+drooschuck@users.noreply.github.com>
2024-11-19 06:03:00 +0000

    Update news_summary.py

--- news_summary.py
+++ news_summary.py
@@ -53,7 +53,7 @@ def scrape_bbc_news():
     url = 'https://www.bbc.co.uk/news'
     response = requests.get(url)
     soup = BeautifulSoup(response.content, 'html.parser')
-    headlines = soup.find('h1', class_='ssrcss-1s9pby4-Heading e10rt3ze0', limit=5)
+    headlines = soup.find_all('h1', class_='ssrcss-1s9pby4-Heading e10rt3ze0', limit=5)
     news_summary = "BBC News Headlines:\n"
     for idx, headline in enumerate(headlines, 1):
         news_summary += f"{idx}. {headline.get_text().strip()}\n"
